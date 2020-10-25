aeb763835c9b7396ac038ba1c18be74a6b2bba49
Rumpl3slaw <wowpanda13@ukr.net>
2020-10-25 14:48:23 +0200

    master commit

---
+++ Lab 4.3/Lab 4.3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	double x, xp, xk, dx, F, a, b, c;
+
+	cout << "a=";cin >> a;
+	cout << "b=";cin >> b;
+	cout << "c=";cin >> c;
+	cout << "xp=";cin >> xp;
+	cout << "xk=";cin >> xk;
+	cout << "dx=";cin >> dx;
+
+
+	cout << fixed;
+	cout << "---------"<<endl;
+	cout << "|" << setw(4) << "F" << "   |" << endl;
+	cout << "---------" << endl;
+
+
+	x = xp;
+	while (x <= xk)
+	{
+		if (x < 0 && b != 0)
+			F = a - (x / 10 + b);
+		else
+			if (x > 0 && b == 0)
+				F = (x - a) / (x - c);
+			else
+				F = (3 * x) + (2 / c);
+
+		cout << "|" << setw(7) << setprecision(2) << F
+			<< "|" << endl;
+
+		x += dx;
+	}
+	cout << "---------";
+	return 0;
+}
